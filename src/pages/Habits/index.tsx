d1152e9fcd99eeecce396917454d8bd80b26166a
RondneyLoiola <rondney.loiola11@gmail.com>
2025-12-26 16:52:07 -0300

    configurando data da tela principal e configurando api.interceptors

--- src/pages/Habits/index.tsx
+++ src/pages/Habits/index.tsx
@@ -1,35 +1,40 @@
-import { PaperPlaneRightIcon, TrashIcon } from "@phosphor-icons/react"
-import SideBar from "../../Components/Siderbar"
-import styles from './styles.module.css'
+import { PaperPlaneRightIcon, TrashIcon } from '@phosphor-icons/react';
+import SideBar from '../../Components/Siderbar';
+import styles from './styles.module.css';
 
 function Habits() {
-    return (
-        <div className={styles.app}>
-            <SideBar />
-            <div className={styles.container}>
-                <div className={styles.content}>
-                    <header>
-                        <h1>Hábitos Diários</h1>
-                        <span>Hoje, 22 de Dezembro</span>
-                    </header>
-                    <div className={styles.input}>
-                        <input type="text" placeholder="Digite aqui um novo hábito..."/>
-                        <PaperPlaneRightIcon />
-                    </div>
-                    <div className={styles.habits}>
-                        <div className={styles.habit}>
-                            <p>Estudar Espanhol</p>
-                            <div>
-                                <input type="checkbox" />
-                                <TrashIcon/>
-                            </div>
-                        </div>
-                    </div>
-                    
-                </div>
-            </div>
-        </div>
-    )
+	return (
+		<div className={styles.app}>
+			<SideBar />
+			<div className={styles.container}>
+				<div className={styles.content}>
+					<header>
+						<h1>Hábitos Diários</h1>
+						<span>
+							{`Hoje, ${new Intl.DateTimeFormat('pt-BR', {
+								dateStyle: 'long',
+								timeZone: 'America/Sao_Paulo',
+							}).format(new Date())}`}
+							
+						</span>
+					</header>
+					<div className={styles.input}>
+						<input type="text" placeholder="Digite aqui um novo hábito..." />
+						<PaperPlaneRightIcon />
+					</div>
+					<div className={styles.habits}>
+						<div className={styles.habit}>
+							<p>Estudar Espanhol</p>
+							<div>
+								<input type="checkbox" />
+								<TrashIcon />
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default Habits
+export default Habits;
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,20 @@
 import axios from 'axios';
+import { userLocalStorageKey } from '../hooks/useUser';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+	baseURL: import.meta.env.VITE_API_URL,
+});
+
+api.interceptors.request.use((config) => {
+	const userData = localStorage.getItem(userLocalStorageKey);
+
+	const token = userData && JSON.parse(userData).token;
+
+	if (token) {
+		config.headers.Authorization = `Bearer ${token}`;
+	}
+
+	return config;
 });
 
 export default api;
